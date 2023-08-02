50959b82b6bf9131a62138da77f05e8a5b3cc789
Mangesh Sodnar <82178071+MangeshSodnar123@users.noreply.github.com>
2023-08-02 08:36:16 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+import UserDashboard from "./session1-takehome/UserDashBoard";
+import DogPics from "./session2-takehome/DogPics";
+import BookAppointment from "./session3-takehome/BookAppointment";
+import WeatherApp from "./Interview-Challenge/WeatherApp";
+
+export default function App() {
+  return (
+    <div className="App">
+      {/* this is for session1  */}
+      {/* <UserDashboard />  */}
+
+      {/* session2 */}
+      {/* <DogPics /> */}
+
+      {/* session3 */}
+      {/* <BookAppointment /> */}
+
+      {/* Interview Challenge */}
+      <WeatherApp />
+    </div>
+  );
+}
